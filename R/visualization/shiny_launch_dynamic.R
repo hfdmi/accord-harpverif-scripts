697d91df571f07481d00c8c9a966bc7e253d1fb0
Samuel Viana-Jimenez <sp3c@ac6-102.bullx>
2023-04-28 11:12:06 +0000

    Include instructions in shiny web launch scripts to indicate the user how to open the app in a web browser window at ATOS.

--- R/visualization/shiny_launch_dynamic.R
+++ R/visualization/shiny_launch_dynamic.R
@@ -2,6 +2,12 @@
 
 library(harpVis)
 library(here)
+port <- Sys.getenv(c("SHINY_PORT"))
+host <- Sys.getenv(c("HOST"))
+print('To display shiny app in Firefox window at ATOS:')
+print('1: open a new terminal')
+print(paste0('execute this command: ssh -L ', port,':localhost:',port,' ',host))
+print(paste0('Open a Firefox window and go to http://127.0.0.1:',port,'/'))
 app_dir <- system.file("shiny_apps/plot_point_verif", package= "harpVis")
 shiny::shinyOptions(app_start_dir=paste( Sys.getenv(c("PERM")),"/deode_verif/",sep=""))
 shiny::runApp(app_dir, port=strtoi(Sys.getenv(c("SHINY_PORT"))))
--- R/visualization/shiny_launch_static.R
+++ R/visualization/shiny_launch_static.R
@@ -2,4 +2,10 @@
 
 library(here)
 library(shiny)
+port <- Sys.getenv(c("SHINY_PORT"))
+host <- Sys.getenv(c("HOST"))
+print('To display shiny app in Firefox window at ATOS:')
+print('1: open a new terminal')
+print(paste0('execute this command: ssh -L ', port,':localhost:',port,' ',host))
+print(paste0('Open a Firefox window and go to http://127.0.0.1:',port,'/'))
 runApp(here('plot_point_verif_local1',port=strtoi(Sys.getenv(c("SHINY_PORT")))))
